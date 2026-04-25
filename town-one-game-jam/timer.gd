39d773f8c1326c3a8caee34b2742016c3ee35aa2
Emvypie <13lisuardi@gmail.com>
2026-04-25 15:37:06 -0700

    Transition to game over scene when timer reaches 0

--- town-one-game-jam/timer.gd
+++ town-one-game-jam/timer.gd
@@ -5,6 +5,7 @@ extends Node
 
 func _ready():
 	timer.start()
+	timer.timeout.connect(_on_timer_timeout)
 
 func time_left():
 	var time_left = timer.time_left
@@ -14,4 +15,6 @@ func time_left():
 
 func _process(delta):
 	label.text = "%02d:%02d" % time_left()
-	
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://GameOver_Scene.tscn")
